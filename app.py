39b2a32d6709f9767a97ad3873f4f3c2cf627e24
krishnanunni br <krishnanunni5307@gmail.com>
2022-12-13 17:39:19 +0530

    setup till logging module and exception before ml code

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from housing.logger import logging
+import sys
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
-    return "starting machine learning project"
+    try:
+        raise Exception("testing new cutoom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)    
+        logging.info(housing.error_message)
+        
+        logging.info("we are tesing logging module")
+    return "starting the machine learning project "
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,33 +1,99 @@
-from setuptools import setup, find_packages
+from setuptools import setup,find_packages
 from typing import List
 
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"                         ## this version of package will get installed using "-e ." through requirements.txt file run
+AUTHOR="krishnanunni"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements_list() -> List[str]:
+
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+
+    ## -e . will install all libraries wherever  __init__ is preesent other than requiremets.txt 
+                                                            ## we remove -e . in setup.py because we used find_packages() funtion below which will return -
+                                                            #- all our packages names in list format
+                                                            ## both find_packages() and -e . does the same thing  
+    """
+
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+    
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(),                     ## our own custom libraries or Returns all packages where there is __init__.py is present  
+install_requires=get_requirements_list()         ## for external linraries
+)
 
 
 
-## DECLARING VARIABLES FOR SETUP FUNCTION
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+
 PROJECT_NAME= "housing-prediction"
-VERSION = "0.0.1"            ## this version of package will get installed using "-e ." through requirements.txt file run 
+VERSION = "0.0.2"             
 AUTHOR = "Krishnanunni"
 DESCRIPTION = "This is first FSDS ML project"
-PACKAGES = ["housing"]     ## specify folder name in packages 
+PACKAGES = ["housing"]    
 REQUIREMENTS_FILE_NAME= "requirements.txt"
+
+
+
 
 def get_requirements_list()->List[str]:
 
-    """
+   
     Description: this function is going to return list of requirement
     mention in requirements.txt file
 
     return this function is going to return a list which contain name of
     libraries mentioned in requirements.txt file
 
-    """
+     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
+   
+    
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove("-e .")  ## -e . will install all libraries wherever  __init__ is preesent other than requiremets.txt 
-                                                            ## we remove -e . in setup.py because we used find_packages() funtion below which will return -
-                                                            #- all our packages names in list format
-                                                            ## both find_packages() and -e . does the same thing
+        return requirement_file.readlines().remove('-e .') 
+    
+
+    
+   
 
 setup(
 
@@ -35,8 +101,8 @@ name= PROJECT_NAME,
 version= VERSION,
 author= AUTHOR,
 description= DESCRIPTION,
-packages= find_packages,                  ## our own custom libraries or Returns all packages where there is __init__.py is present  
-install_requires= get_requirements_list()   ## for external linraries
+packages= find_packages(),                 
+install_requires= get_requirements_list(),  
 
 )
 
@@ -45,3 +111,7 @@ install_requires= get_requirements_list()   ## for external linraries
 
 if __name__=="__main__":
     print(get_requirements_list())
+
+    
+    
+"""

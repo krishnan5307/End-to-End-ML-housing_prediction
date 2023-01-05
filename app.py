a5fd7185967c868672bf5c5a1bf2e4cd24974a9d
krishnanunni br <krishnanunni5307@gmail.com>
2023-01-05 23:58:14 +0530

    updated

--- app.py
+++ app.py
@@ -1,20 +1,130 @@
-from flask import Flask
-from housing.logger import logging
+from flask import Flask, request
 import sys
+
+import pip
+from housing.util.util import read_yaml_file, write_yaml_file
+from matplotlib.style import context
+from housing.logger import logging
 from housing.exception import HousingException
+import os, sys
+import json
+from housing.config.configuration import Configuartion
+from housing.constant import CONFIG_DIR, get_current_time_stamp
+from housing.pipeline.pipeline import Pipeline
+from housing.entity.housing_predictor import HousingPredictor, HousingData
+from flask import send_file, abort, render_template
+
+
+ROOT_DIR = os.getcwd()
+LOG_FOLDER_NAME = "logs"
+PIPELINE_FOLDER_NAME = "housing"
+SAVED_MODELS_DIR_NAME = "saved_models"
+MODEL_CONFIG_FILE_PATH = os.path.join(ROOT_DIR, CONFIG_DIR, "model.yaml")
+LOG_DIR = os.path.join(ROOT_DIR, LOG_FOLDER_NAME)
+PIPELINE_DIR = os.path.join(ROOT_DIR, PIPELINE_FOLDER_NAME)
+MODEL_DIR = os.path.join(ROOT_DIR, SAVED_MODELS_DIR_NAME)
+
+
+from housing.logger import get_log_dataframe
+
+HOUSING_DATA_KEY = "housing_data"
+MEDIAN_HOUSING_VALUE_KEY = "median_house_value"
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     try:
-        raise Exception("testing new cutoom exception")
+        return render_template('index.html')
     except Exception as e:
-        housing = HousingException(e,sys)    
-        logging.info(housing.error_message)
-        
-        logging.info("we are tesing logging module")
-    return "starting the machine learning project "
+        return str(e)
+
+
+
+
+@app.route('/train', methods=['GET', 'POST'])
+def train():
+    message = ""
+    pipeline = Pipeline(config=Configuartion(current_time_stamp=get_current_time_stamp()))
+    if not Pipeline.experiment.running_status:
+        message = "Training started."
+        pipeline.start()
+    else:
+        message = "Training is already in progress."
+    context = {
+        "experiment": pipeline.get_experiments_status().to_html(classes='table table-striped col-12'),
+        "message": message
+    }
+    return render_template('train.html', context=context)
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    context = {
+        HOUSING_DATA_KEY: None,
+        MEDIAN_HOUSING_VALUE_KEY: None
+    }
+
+    if request.method == 'POST':
+        longitude = float(request.form['longitude'])
+        latitude = float(request.form['latitude'])
+        housing_median_age = float(request.form['housing_median_age'])
+        total_rooms = float(request.form['total_rooms'])
+        total_bedrooms = float(request.form['total_bedrooms'])
+        population = float(request.form['population'])
+        households = float(request.form['households'])
+        median_income = float(request.form['median_income'])
+        ocean_proximity = request.form['ocean_proximity']
+
+        housing_data = HousingData(longitude=longitude,     ## creating object of housing class in housing_predictor.py
+                                   latitude=latitude,
+                                   housing_median_age=housing_median_age,
+                                   total_rooms=total_rooms,
+                                   total_bedrooms=total_bedrooms,
+                                   population=population,
+                                   households=households,
+                                   median_income=median_income, 
+                                   ocean_proximity=ocean_proximity,
+                                   )
+        housing_df = housing_data.get_housing_input_data_frame()  ## calling function inside hosuing class
+        housing_predictor = HousingPredictor(model_dir=MODEL_DIR)      ## creating an object with intialization as model_dir
+        median_housing_value = housing_predictor.predict(X=housing_df)   ## using the above obj to do preiction
+        context = {
+            HOUSING_DATA_KEY: housing_data.get_housing_data_as_dict(), ## FOR PASSING TO HTML PAGE VIA CONTEXT
+            MEDIAN_HOUSING_VALUE_KEY: median_housing_value,
+        }
+        return render_template('predict.html', context=context)
+    return render_template("predict.html", context=context)
+
+
+@app.route('/saved_models', defaults={'req_path': 'saved_models'})
+@app.route('/saved_models/<path:req_path>')
+def saved_models_dir(req_path):
+    os.makedirs("saved_models", exist_ok=True)
+    # Joining the base and the requested path
+    print(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)             ## downloaidng file if its a file like model.pkl
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}   ## else go in inside folder
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('saved_models_files.html', result=result)
+
+
+
 
 
 if __name__ == "__main__":
